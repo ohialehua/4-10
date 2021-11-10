881a05ff5e905bb54b0166d6208b19bb0c897a7e
EC2 Default User <ec2-user@ip-172-31-79-219.ec2.internal>
2021-11-10 01:56:44 +0000

    ADD ルーティング,コントローラ,モデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_233424) do
+ActiveRecord::Schema.define(version: 2021_11_10_010057) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "book_title"
+    t.text "book_opinion"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
